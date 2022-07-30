cb9045ab95f59e790b8435758a91cc79e2cf28f0
Veanty <plmulticraftpl@gmail.com>
2022-07-30 16:41:21 +0200

    Introduce Function type callbacks

--- 02/app.ts
+++ 02/app.ts
@@ -6,6 +6,15 @@ function printResult(num: number) {
   console.log(`Result ${num}`);
 }
 
+function addAndHandle(
+  number1: number,
+  number2: number,
+  cb: (number: number) => void
+) {
+  const result = number1 + number2;
+  cb(result);
+}
+
 printResult(add(5, 12));
 
 let combineValues: (number1: number, number2: number) => number;
@@ -13,3 +22,7 @@ let combineValues: (number1: number, number2: number) => number;
 combineValues = add;
 
 console.log(combineValues(8, 8));
+
+addAndHandle(10, 20, (result) => {
+  console.log(result);
+});
