68e7cc42647b6d4a911b122d47cd82fb4ec4c9b8
Veanty <plmulticraftpl@gmail.com>
2022-07-30 14:39:01 +0200

    Refactor: array types explained

--- 02/app.ts
+++ 02/app.ts
@@ -1,6 +1,11 @@
 const person = {
   name: "Maximilian",
   age: 30,
+  hobbies: ["Sports", "Cooking"],
 };
 
 console.log(person.name);
+
+for (let hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
