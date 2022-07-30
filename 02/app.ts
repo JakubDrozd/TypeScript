030c4a179d13cb493a4834d6da956ef72f351be8
Veanty <plmulticraftpl@gmail.com>
2022-07-30 16:47:07 +0200

    Introduce 'unknown' type

--- 02/app.ts
+++ 02/app.ts
@@ -1,28 +1,9 @@
-function add(n1: number, n2: number): number {
-  return n1 + n2;
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = "Max";
+
+if (typeof userInput === "string") {
+  userName = userInput;
 }
-
-function printResult(num: number) {
-  console.log(`Result ${num}`);
-}
-
-function addAndHandle(
-  number1: number,
-  number2: number,
-  cb: (number: number) => void
-) {
-  const result = number1 + number2;
-  cb(result);
-}
-
-printResult(add(5, 12));
-
-let combineValues: (number1: number, number2: number) => number;
-
-combineValues = add;
-
-console.log(combineValues(8, 8));
-
-addAndHandle(10, 20, (result) => {
-  console.log(result);
-});
