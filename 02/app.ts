0f565362ba53bd040f2402f0b62bd3d6f7d73583
Veanty <plmulticraftpl@gmail.com>
2022-07-30 15:08:44 +0200

    Introduce Union Type

--- 02/app.ts
+++ 02/app.ts
@@ -1,38 +1,15 @@
-// const person: {
-//   name: string;
-//   age: number;
-//   hobbies: string[];
-//   role: [number, string];
-// } = {
-//   name: "Maximilian",
-//   age: 30,
-//   hobbies: ["Sports", "Cooking"],
-//   role: [2, "author"],
-// };
-
-// const ADMIN = 0;
-// const READ_ONLY = 1;
-// const AUTHOR = 2;
-
-enum Role {
-  ADMIN = "ADMIN",
-  READ_ONLY = 100,
-  AUTHOR = "AUTHOR",
+function combine(input1: number | string, input2: number | string) {
+  let result;
+  if (typeof input1 === "number" && typeof input2 === "number") {
+    result = input1 + input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  return result;
 }
 
-const person = {
-  name: "Maximilian",
-  age: 30,
-  hobbies: ["Sports", "Cooking"],
-  role: Role.ADMIN,
-};
+const combineAges = combine(30, 26);
+console.log(combineAges);
 
-console.log(person.name);
-
-for (let hobby of person.hobbies) {
-  console.log(hobby.toUpperCase());
-}
-
-if (person.role === Role.ADMIN) {
-  console.log(`is admin`);
-}
+const combineNames = combine("Max", "Anna");
+console.log(combineNames);
