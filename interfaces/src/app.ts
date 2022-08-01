9faf40d77f584c520a20144f5e0f09db07cf4840
Veanty <plmulticraftpl@gmail.com>
2022-08-01 17:52:13 +0200

    Class Inheritace introduce

--- interfaces/src/app.ts
+++ interfaces/src/app.ts
@@ -1,12 +1,9 @@
 class Department {
-  name: string = "DEFAULT";
   private employees: string[] = [];
-  constructor(n: string) {
-    this.name = n;
-  }
+  constructor(private readonly id: string, public name: string) {}
 
   describe(this: Department) {
-    console.log(`Department: ${this.name}`);
+    console.log(`Department: (${this.id}) ${this.name}`);
   }
 
   addEmployee(employee: string) {
@@ -18,8 +15,40 @@ class Department {
   }
 }
 
-const accounting = new Department("Accounting");
+class ITDepartment extends Department {
+  constructor(id: string, public admins: string[]) {
+    super(id, "IT");
+  }
+}
 
+const accounting = new Department("d1", "Accounting");
+
+const it = new ITDepartment("d2", ["Jakub, Piotr"]);
+
+it.describe();
+it.addEmployee("Jakub");
+it.addEmployee("Piotr");
+it.printEmployee();
+
+accounting.describe();
 accounting.addEmployee("Jakub");
 accounting.addEmployee("Piotr");
 accounting.printEmployee();
+
+class AccountingDepartment extends Department {
+  constructor(id: string, private reports: string[]) {
+    super(id, "Accounting");
+  }
+
+  addReport(text: string) {
+    this.reports.push(text);
+  }
+
+  getReports() {
+    console.log(this.reports);
+  }
+}
+
+const accounting2 = new AccountingDepartment("d3", []);
+accounting2.addReport("Something went wrong");
+accounting2.getReports();
