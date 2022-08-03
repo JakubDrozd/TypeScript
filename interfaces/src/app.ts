436c5d415e349746a6b27436922a23dded7bc2a5
Veanty <plmulticraftpl@gmail.com>
2022-08-03 18:06:53 +0200

    Interface + Class implementation

--- interfaces/src/app.ts
+++ interfaces/src/app.ts
@@ -1,18 +1,24 @@
-interface Person {
+interface Greetable {
   name: string;
   age: number;
-
   greet(phrase: string): void;
 }
 
-let user1: Person;
-
-user1 = {
-  name: "Max",
-  age: 30,
+class Person implements Greetable {
+  name: string;
+  age = 30;
+  constructor(name: string) {
+    this.name = name;
+  }
   greet(phrase: string) {
     console.log(`${phrase} ${this.name}`);
-  },
-};
+  }
+}
+
+let user1: Greetable;
+
+user1 = new Person("Max");
 
 user1.greet("Hi there I'm");
+
+console.log(user1);
