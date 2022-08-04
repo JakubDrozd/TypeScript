17a1d923f9e0b9160843aa1b6296cd6d4b3a003f
Veanty <plmulticraftpl@gmail.com>
2022-08-04 17:32:39 +0200

    Introduce a Generic Function

--- Generics/src/app.ts
+++ Generics/src/app.ts
@@ -1,3 +1,4 @@
+//GENERICS
 const names: Array<string> = ["Max", "Manuel"];
 names[0].split(" ");
 
@@ -5,8 +6,19 @@ const promise: Promise<string> = new Promise((resolve, reject) => {
   setTimeout(() => {
     resolve("This is done");
   }, 2000);
+
+  reject("...");
 });
 
 promise.then((data) => {
   data.split(" ");
 });
+
+//CREATING A GENERIC FUNCTION
+function merge<T, U>(a: T, b: U) {
+  return Object.assign(a, b);
+}
+
+const mergedObj = merge({ name: "Max" }, { age: 30 });
+
+console.log(mergedObj);
