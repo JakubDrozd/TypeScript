ada95f7d3d26db44e48da1c2ea952f32f0be8bca
Veanty <plmulticraftpl@gmail.com>
2022-08-04 17:44:17 +0200

    Feat: Creating 'countAndDesc' Generic Function

--- Generics/src/app.ts
+++ Generics/src/app.ts
@@ -22,3 +22,15 @@ function merge<T extends object, U extends object>(a: T, b: U) {
 const mergedObj = merge({ name: "Max", hobbies: ["Sports"] }, { age: 30 });
 
 console.log(mergedObj);
+
+function countAndDesc<T extends string>(element: T): [T, string] {
+  let descriptionText = "Got no value.";
+  if (element.length > 0) {
+    descriptionText = `Got ${element.length} elements`;
+  } else if (element.length === 1) {
+    descriptionText = `Got ${element} element.`;
+  }
+  return [element, descriptionText];
+}
+
+console.log(countAndDesc("Kaczanas!"));
