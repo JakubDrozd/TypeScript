0c339946fe7027a2b1d6580eb551eef8d3168c15
Veanty <plmulticraftpl@gmail.com>
2022-08-05 16:48:59 +0200

    Building more useful decorator

--- Decorators/src/app.ts
+++ Decorators/src/app.ts
@@ -1,11 +1,25 @@
 //CLASS DECORATOR
 
-function Logger(constructor: Function) {
-  console.log(`Logging...`);
-  console.log(constructor);
+function Logger(logString: string) {
+  return function (constructor: Function) {
+    console.log(logString);
+    console.log(constructor);
+  };
 }
 
-@Logger
+function WithTemplate(template: string, hookID: string) {
+  return function (constructor: any) {
+    const hookEl = document.getElementById(hookID);
+    const p = new constructor();
+    if (hookEl) {
+      hookEl.innerHTML = template;
+      hookEl.querySelector("h1")!.textContent = p.name;
+    }
+  };
+}
+
+// @Logger("LOGGING - PERSON")
+@WithTemplate("<h1>My Person Object</h1>", "app")
 class Person {
   name = "Max";
   constructor() {
