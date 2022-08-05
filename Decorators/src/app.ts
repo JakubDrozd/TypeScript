268715301cf1c1964241187d4a4e47ae7e408432
Veanty <plmulticraftpl@gmail.com>
2022-08-05 17:05:25 +0200

    Adding property decorator

--- Decorators/src/app.ts
+++ Decorators/src/app.ts
@@ -33,3 +33,32 @@ class Person {
 const p1 = new Person();
 
 console.log(p1);
+
+// ----
+
+function Log(target: any, propertyName: string | Symbol) {
+  console.log(`Property decorator`);
+  console.log(target, propertyName);
+}
+
+class Product {
+  @Log
+  title: string;
+  private _price: number;
+
+  setPrice(value: number) {
+    if (value > 0) {
+      this._price = value;
+    } else {
+      throw new Error("Invalid price: should be positive");
+    }
+  }
+
+  constructor(title: string, price: number) {
+    this.title = title;
+    this._price = price;
+  }
+  getPriceTax(tax: number) {
+    return this._price * (1 + tax);
+  }
+}
