16e3350a15798dbbaf6808b70c3e31460270e843
Veanty <plmulticraftpl@gmail.com>
2022-08-06 16:45:52 +0200

    Add parameter, method and accessor decorators

--- Decorators2/src/app.ts
+++ Decorators2/src/app.ts
@@ -1,35 +1,75 @@
-function CarDecorator(constructor: Function) {
-  constructor.prototype.date = new Date();
-  constructor.prototype.giveMeDate = function () {
-    alert(this.date);
-  };
+// function CarDecorator(constructor: Function) {
+//   constructor.prototype.date = new Date();
+//   constructor.prototype.giveMeDate = function () {
+//     alert(this.date);
+//   };
+// }
+
+// function OtherDecorator(name: string) {
+//   if (name === "Mike") {
+//     return function (constructor: Function) {
+//       constructor.prototype.other = "Other Value";
+//     };
+//   } else {
+//     throw new Error("Wrong name");
+//   }
+// }
+
+// @OtherDecorator("Mike")
+// @CarDecorator
+// class Car {
+//   brand: string;
+//   constructor(brand: string) {
+//     this.brand = brand;
+//   }
+// }
+
+// @CarDecorator
+// class Vehicle {}
+// const vehicle = new Vehicle();
+// console.log("vehicle date :", (<any>vehicle).date);
+
+// const car = new Car("Ford");
+// console.log(car.brand);
+// console.log((<any>car).date);
+// console.log((<any>car).giveMeDate);
+
+function PrintDecorator(
+  target: any,
+  property: string,
+  descriptor: PropertyDescriptor
+) {
+  console.log(target, property, descriptor);
+  descriptor.writable = false;
 }
 
-function OtherDecorator(name: string) {
-  if (name === "Mike") {
-    return function (constructor: Function) {
-      constructor.prototype.other = "Other Value";
-    };
-  } else {
-    throw new Error("Wrong name");
+function NameProperty(target: any, propertyKey: string) {
+  console.log(target, propertyKey);
+}
+
+function ParameterDecorator(
+  target: any,
+  propertyKey: string,
+  parameterIndex: number
+) {
+  console.log(target, propertyKey, parameterIndex);
+}
+
+class Tool {
+  @NameProperty
+  name: string;
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  price() {
+    console.log("$ 200");
+  }
+
+  @PrintDecorator
+  print(isTrue: string, @ParameterDecorator printNumber: number) {
+    console.log(`Printing tool`);
   }
 }
 
-@OtherDecorator("Mike")
-@CarDecorator
-class Car {
-  brand: string;
-  constructor(brand: string) {
-    this.brand = brand;
-  }
-}
-
-@CarDecorator
-class Vehicle {}
-const vehicle = new Vehicle();
-console.log("vehicle date :", (<any>vehicle).date);
-
-const car = new Car("Ford");
-console.log(car.brand);
-console.log((<any>car).date);
-console.log((<any>car).giveMeDate);
+const tool = new Tool("Hammer");
