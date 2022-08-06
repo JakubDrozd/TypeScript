94f64b0bcac0492370fa7ec85647c60605675b79
Veanty <plmulticraftpl@gmail.com>
2022-08-06 15:13:13 +0200

    Add decorator to multiple classes and injecting properites and method

---
+++ Decorators2/src/app.ts
@@ -0,0 +1,24 @@
+function CarDecorator(constructor: Function) {
+  constructor.prototype.date = new Date();
+  constructor.prototype.giveMeDate = function () {
+    alert(this.date);
+  };
+}
+
+@CarDecorator
+class Car {
+  brand: string;
+  constructor(brand: string) {
+    this.brand = brand;
+  }
+}
+
+@CarDecorator
+class Vehicle {}
+const vehicle = new Vehicle();
+console.log("vehicle date :", (<any>vehicle).date);
+
+const car = new Car("Ford");
+console.log(car.brand);
+console.log((<any>car).date);
+console.log((<any>car).giveMeDate);
