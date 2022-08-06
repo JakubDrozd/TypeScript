f3b459bdce4c31f3ca59f049a05b0443f385b6f9
Veanty <plmulticraftpl@gmail.com>
2022-08-06 17:47:58 +0200

    Add event listener to submit button and handler methods to main class

--- TSProject/src/app.ts
+++ TSProject/src/app.ts
@@ -2,6 +2,9 @@ class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
   constructor() {
     this.templateElement = <HTMLTemplateElement>(
@@ -15,8 +18,30 @@ class ProjectInput {
     );
 
     this.element = <HTMLFormElement>importedNode.firstElementChild;
+    this.element.id = "user-input";
+    this.titleInputElement = <HTMLInputElement>(
+      this.element.querySelector("#title")!
+    );
+    this.descriptionInputElement = <HTMLInputElement>(
+      this.element.querySelector("#description")!
+    );
+    this.peopleInputElement = <HTMLInputElement>(
+      this.element.querySelector("#people")!
+    );
+
+    this.configure();
     this.attach();
   }
+
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+
+  private configure() {
+    this.element.addEventListener("submit", this.submitHandler.bind(this));
+  }
+
   private attach() {
     this.hostElement.insertAdjacentElement("afterbegin", this.element);
   }
