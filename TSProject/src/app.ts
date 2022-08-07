1457221eccc3f506e5a74318deef3f140fe82c73
Veanty <plmulticraftpl@gmail.com>
2022-08-07 10:51:44 +0200

    Add decortator for binding the function to eventListener

--- TSProject/src/app.ts
+++ TSProject/src/app.ts
@@ -1,3 +1,19 @@
+function AutoBind(
+  target: any,
+  methodName: string,
+  descriptor: PropertyDescriptor
+) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
+}
+
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
@@ -33,13 +49,14 @@ class ProjectInput {
     this.attach();
   }
 
+  @AutoBind
   private submitHandler(event: Event) {
     event.preventDefault();
     console.log(this.titleInputElement.value);
   }
 
   private configure() {
-    this.element.addEventListener("submit", this.submitHandler.bind(this));
+    this.element.addEventListener("submit", this.submitHandler);
   }
 
   private attach() {
