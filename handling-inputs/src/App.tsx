3cbb803086a5a0b19810636d98f6383b950f4ca6
Veanty <plmulticraftpl@gmail.com>
2022-08-15 15:54:15 +0200

    Feat: Add basic form input handling

---
+++ handling-inputs/src/App.tsx
@@ -0,0 +1,37 @@
+import { useState } from "react"
+import Button from "react-bootstrap/Button"
+import Form from "react-bootstrap/Form"
+
+export function App() {
+  const [state, setState] = useState({
+    email: "",
+    password: ""
+  })
+  const handleChange = (e:any) =>{
+    setState({
+      ...state,
+      [e.target.name]: e.target.value
+    })
+  }
+  return(
+    <div>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control type="email" name="email" onChange={handleChange}></Form.Control>
+          <Form.Text className="text-muted"></Form.Text>
+        </Form.Group>
+        
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" name="password" onChange={handleChange}></Form.Control>
+        </Form.Group>
+        <Button variant="primary" as="input" type="submit" value="Submit"></Button>
+        
+      </Form>
+      <h5>{state.email} {state.password}</h5>
+    </div>
+  )
+}
+
+export default App
