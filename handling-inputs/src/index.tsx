7658bc9d8ecd5c20b49714599fa50c1bb8118830
Veanty <plmulticraftpl@gmail.com>
2022-08-17 17:55:24 +0200

    Refactor: Change render source to Clock component

--- handling-inputs/src/index.tsx
+++ handling-inputs/src/index.tsx
@@ -4,16 +4,16 @@ import './styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { App2 } from './components/App2';
+import { Clock } from './components/Clock';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App2 />
-  </React.StrictMode>
-);
 
+root.render(
+<Clock>
+</Clock>
+)
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
