6da5c1628c62d3f976972374c8cc783a77fb6244
Veanty <plmulticraftpl@gmail.com>
2022-08-18 16:51:48 +0200

    Feat: Add new lifecycle functions to React Component

--- lifecycle-hooks/src/Counter.tsx
+++ lifecycle-hooks/src/Counter.tsx
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import './App.css';
 import Button from "react-bootstrap/Button"
 
+// const ErrorComponent = ()=>{
+//   return <div>{props.ignore}</div>
+// }
 
 export class Counter extends Component<any, any> {
   increment: () => void;
@@ -18,11 +21,32 @@ export class Counter extends Component<any, any> {
       this.decrement = () => this.setState({counter: this.state.counter - 1})
   }
 
+  static getDerivedStateFromProps(props: any, state: any){
+    if(props.seed && state.seed !== props.seed){
+      return {seed: props.seed, counter: props.seed}
+    }
+    return null
+  }
+
   componentDidMount(){
     console.log("Component mounted!")
   }
   
+  shouldComponentUpdate(nextProps: any, nextState: any){
+    if(nextProps.ignoreProp && this.props.ignoreProp !== nextProps.ignoreProp){
+      return false
+    }
+    return true
+  }
+
+  getSnapshotBeforeUpdate(prevProps: any, prevState: any){
+    return null
+  }
+
   render() {
+    if(this.state.error){
+      return <div>Encountered an error ({this.state.error})</div>
+    }
     return(
       <div>
         <div className="counter">
@@ -30,6 +54,7 @@ export class Counter extends Component<any, any> {
         </div>
         <Button className="m-3" onClick={this.increment}>Increment</Button>
         <Button className="m-3" onClick={this.decrement}>Decrement</Button>
+        {/* <ErrorComponent></ErrorComponent> */}
       </div>
     )
   }
@@ -40,6 +65,12 @@ export class Counter extends Component<any, any> {
 
   componentWillUnmount(){
     console.log("Component Will UnMount!")
+  }
+
+  componentDidCatch(error: any, info: any){
+    console.log("Component did catch");
+
+    this.setState({error, info})
   }
 }
 
