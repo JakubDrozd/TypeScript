d1cfae575664bf5974fcd419bb195d9d6873b578
Veanty <plmulticraftpl@gmail.com>
2022-08-18 16:51:28 +0200

    Feat: Add new lifcycle function to React Components

--- lifecycle-hooks/src/App.tsx
+++ lifecycle-hooks/src/App.tsx
@@ -7,22 +7,32 @@ import Button from "react-bootstrap/Button"
 export class App extends Component<any, any> {
     mountCounter: () => void;
     unMountCounter: () => void;
+    ignoreProp: () => void;
+    seedGenerator: () => void;
     constructor(props: any){
         super(props)
         this.state = {
             mount: true,
+            ignoreProp: 0,
+            seed: 40
         }
 
         this.mountCounter = () => this.setState({mount: true})
 
         this.unMountCounter = () => this.setState({mount: false})
+
+        this.ignoreProp = () => this.setState({ignoreProp: Math.random()})
+
+        this.seedGenerator = () => this.setState({seed: Math.random()*100})
     }
     render(){
         return(
             <div>
-                {this.state.mount ? <Counter></Counter> : null}
+                {this.state.mount ? <Counter ignore={this.state.ignoreProp} seed={this.state.seed}></Counter> : null}
                 <Button variant="info" className="m-3" onClick={this.mountCounter} disabled={this.state.mount}>Mount Counter</Button>
+                <Button className="m-3" onClick={this.ignoreProp}>Ignore Prop</Button>
                 <Button variant="danger" className="m-3" onClick={this.unMountCounter} disabled={!this.state.mount}>UnMount Counter</Button>
+                <Button onClick={this.seedGenerator} className="m-3" >Generate Seed</Button>
             </div>
         )
     }
